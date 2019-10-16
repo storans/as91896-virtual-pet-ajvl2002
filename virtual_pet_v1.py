0e7a2e86942737e7511601977286a16c14549e57
AvanLoo <avanloo@storans.school.nz>
2019-10-17 11:53:57 +1300

    initial commit after set up problems

---
+++ virtual_pet_v1.py
@@ -0,0 +1,14 @@
+# welcomes the user to the virtual pet program
+print("Welcome to Virtual Pet!")
+print()
+print("Having a virtual pet is great if you are allergic or not allowed to have pets.")
+
+# minimum and maximum weight of the virtual pet
+minimum_weight = 1.5
+maximum_weight = 2.5
+
+food_values = {"carrot": 0.2, "broccoli": 0.2, "kale": 0.1, "grass": 0.3}
+
+print()
+print("Main menu")
+print("Please select a number from the list below and type the number into the space provided")
