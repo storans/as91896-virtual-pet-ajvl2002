b7ab779ecbc6123d894de04e13135dc13c63db02
AvanLoo <avanloo@storans.school.nz>
2019-10-18 12:05:51 +1300

    name the pet

---
+++ name_pet.py
@@ -0,0 +1,10 @@
+name = input("Please choose a name for your virtual pet Rabbit:")
+try:
+    val = int(name)
+    print("Please choose a name.")
+except ValueError:
+   print("That is a great name for your virtual pet rabbit! ")
+
+
+
+
