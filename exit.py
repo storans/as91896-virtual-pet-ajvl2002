19975c742c5e9dab64ce1aa0bfc1b879071a4285
AvanLoo <avanloo@storans.school.nz>
2019-10-23 13:54:27 +1300

    name the virtual pet component

---
+++ exit.py
@@ -0,0 +1,10 @@
+print("1. Check your virtual pet's weight\n"
+      "2. feed your virtual pet\n"
+      "3. Exercise your virtual pet\n"
+      "4. Help\n"
+      "5. Exit virtual pet\n")
+menu_choice = int(input("Enter your choice:"))
+
+if menu_choice == 5:
+
+
