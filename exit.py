b5eecb87ac57904eb96ba79ef11f7967e93e8c1f
AvanLoo <avanloo@storans.school.nz>
2019-10-28 21:22:50 +1300

    Exit game component

--- exit.py
+++ exit.py
@@ -1,10 +1,36 @@
-print("1. Check your virtual pet's weight\n"
-      "2. feed your virtual pet\n"
-      "3. Exercise your virtual pet\n"
-      "4. Help\n"
-      "5. Exit virtual pet\n")
-menu_choice = int(input("Enter your choice:"))
+def check_int(question, error, low, high):
+    valid = False
 
-if menu_choice == 5:
+    while valid == False:
+        number = input("{}".format(question))
+        try:
+            number = int(number)
+            if low <= number <= high:
+                return number
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+tracking_on = True
+while tracking_on == True:
+    while True:
+
+      print("1. Check your virtual pet's weight\n"
+            "2. feed your virtual pet\n"
+            "3. Exercise your virtual pet\n"
+            "4. Help\n"
+            "5. Exit virtual pet\n")
+      menu_choice = check_int("Enter the number of your choice:", "Please choose a number between 1 and 5.", 1, 5)
+      print()
+
+      if menu_choice == 5:
+            tracking_on = False
+            print("Thanks for playing!")
+            break
+      else:
+          print("Please enter another number")
+          print()
+
 
 
