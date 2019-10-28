32d2fa9495236d72a76bd90067c3b0a86eaf3ab4
AvanLoo <avanloo@storans.school.nz>
2019-10-29 08:09:30 +1300

    Main menu component - asks user to enter the number of their option

--- main_menu.py
+++ main_menu.py
@@ -1,10 +1,10 @@
 # main menu
-food_values = {"carrot": 0.2, "broccoli": 0.2, "kale": 0.1, "grass": 0.3}
-exercise_values = {"hop": 0.2, "run": 0.3, "walk": 0.1}
 
 print("1. Check your virtual pet's weight\n"
       "2. feed your virtual pet\n"
       "3. Exercise your virtual pet\n"
       "4. Help\n"
       "5. Exit virtual pet\n")
+menu_choice = int(input("Please enter the number of the option you wish to do:"))
+print()
 
