f913335462bce9e1f0865ffc074cb7c84453cd1e
AvanLoo <avanloo@storans.school.nz>
2019-10-30 08:21:45 +1300

    Main menu - with check_int

--- main_menu.py
+++ main_menu.py
@@ -1,10 +1,24 @@
-# main menu
+def check_int(question, error, low, high):
+    valid = False
 
+    while valid == False:
+        number = input("{}".format(question))
+        try:
+            number = int(number)
+            if low <= number <= high:
+                return number
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+# main menu
 print("1. Check your virtual pet's weight\n"
       "2. feed your virtual pet\n"
       "3. Exercise your virtual pet\n"
       "4. Help\n"
       "5. Exit virtual pet\n")
-menu_choice = int(input("Please enter the number of the option you wish to do:"))
+menu_choice = check_int("Enter the number of your choice:", "Please choose a number between 1 and 5.", 1, 5)
 print()
+
 
